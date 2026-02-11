4a4868c6e7b190c56b67aa739416b3df4154aeb4
521qinmi <521qinmi@163.com>
2026-02-11 14:00:30 +0800

    Update CanvasController.java

--- src/main/java/com/example/sfdc/controller/CanvasController.java
+++ src/main/java/com/example/sfdc/controller/CanvasController.java
@@ -18,4 +18,13 @@ public class CanvasController {
                            @RequestParam String instanceUrl) {
         return service.getUserInfo(token, instanceUrl);
     }
+    
+    @PostMapping("/sf/canvas")
+    public String canvasEntry(@RequestParam("signed_request") String signedRequest) {
+
+        System.out.println("Canvas 请求到了！");
+        System.out.println("signed_request = " + signedRequest);
+
+        return "<h2>Canvas 已成功连接到你的 Java 服务器 🎉</h2>";
+    }
 }
